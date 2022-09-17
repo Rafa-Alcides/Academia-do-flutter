ec5d0d7257995424972b1dd6772ab1abcec24797
Rafael Alcides <rafalcides@icloud.com>
2022-09-16 21:52:10 -0300

    apreendendo do while

---
+++ Dart-Fundaments/dart_fundamentos/lib/6 - loop/while_do_while.dart
@@ -0,0 +1,19 @@
+void main() {
+  //while
+  var numero = 0;
+  while (numero <= 10) {
+    print(numero);
+    numero++;
+  }
+
+  // Do while
+  // ! mesmo que a condicao for falsa execulta pelo menos uma vez
+  var indece = 0;
+  while (indece > 0) {
+    print(indece);
+    indece++;
+  }
+  do {
+    print(indece);
+  } while (indece > 0);
+}
